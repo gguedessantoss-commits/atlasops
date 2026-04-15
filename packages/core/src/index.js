1836e402b54abbbd0eaf760eb25ff4bd0654a5c9
Gabriel Santos <276027726+gguedessantoss-commits@users.noreply.github.com>
2026-04-15 13:37:46 -0300

    chore: remove generated artifacts

--- packages/core/src/index.js
+++
@@ -1,8 +0,0 @@
-export {
-  buildDashboardSummary,
-  DashboardActivitySchema,
-  DashboardIncidentSchema,
-  DashboardMetricSchema,
-  DashboardServiceHealthSchema,
-  DashboardSummarySchema,
-} from "./dashboard";
